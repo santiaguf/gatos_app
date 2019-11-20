56f8054c03344ff034ccb61912437b269ea15210
santiago_oficina <exactlimon@gmail.com>
2019-11-20 16:18:45 -0500

    Ver gatos favoritos

--- src/main/java/com/platzi/gatos_app/GatosService.java
+++ src/main/java/com/platzi/gatos_app/GatosService.java
@@ -135,14 +135,63 @@ public class GatosService {
         if(gatosArray.length > 0){
             int min = 1;
             int max  = gatosArray.length;
-            int aleatorio = (int) (Math.random() * ((max-min)-1)) + min;
+            int aleatorio = (int) (Math.random() * ((max-min)+1)) + min;
             int indice = aleatorio-1;
             
             GatosFav gatofav = gatosArray[indice];
             
+                //redimensionar en caso de necesitar
+                Image image = null;
+                try{
+                    URL url = new URL(gatofav.image.getUrl());
+                    image = ImageIO.read(url);
+
+                    ImageIcon fondoGato = new ImageIcon(image);
+
+                    if(fondoGato.getIconWidth() > 800){
+                        //redimensionamos
+                        Image fondo = fondoGato.getImage();
+                        Image modificada = fondo.getScaledInstance(800, 600, java.awt.Image.SCALE_SMOOTH);
+                        fondoGato = new ImageIcon(modificada);
+                    }
+
+                    String menu = "Opciones: \n"
+                            + " 1. ver otra imagen \n"
+                            + " 2. Eliminar Favorito \n"
+                            + " 3. Volver \n";
+
+                    String[] botones = { "ver otra imagen", "eliminar favorito", "volver" };
+                    String id_gato = gatofav.getId();
+                    String opcion = (String) JOptionPane.showInputDialog(null,menu,id_gato, JOptionPane.INFORMATION_MESSAGE, fondoGato, botones,botones[0]);
+
+                    int seleccion = -1;
+                    //validamos que opcion selecciona el usuario
+                    for(int i=0;i<botones.length;i++){
+                        if(opcion.equals(botones[i])){
+                            seleccion = i;
+                        }
+                    }
+
+                    switch (seleccion){
+                        case 0:
+                            verFavorito(apikey);
+                            break;
+                        case 1:
+                            borrarFavorito(gatofav);
+                            break;
+                        default:
+                            break;
+                    }
+
+                }catch(IOException e){
+                       System.out.println(e);
+                }
             
         }
         
     }
 
+    public static void borrarFavorito(GatosFav gatofav){
+        
+    }
 }
