8e0c0d4a575276835b14cfb9b14c9b6b95aadbf8
santiago_oficina <exactlimon@gmail.com>
2019-11-20 10:34:09 -0500

    traer datos api, convertir a objeto, y redimesionar imagen

--- src/main/java/com/platzi/gatos_app/GatosService.java
+++ src/main/java/com/platzi/gatos_app/GatosService.java
@@ -5,14 +5,68 @@
  */
 package com.platzi.gatos_app;
 
+import com.google.gson.Gson;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import java.awt.Image;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author ohmyfi
  */
 public class GatosService {
     
-    public static void verGatos(){
+    public static void verGatos() throws IOException{
+        //1. vamos a traer los datos de la API
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url("https://api.thecatapi.com/v1/images/search").get().build();
+
+        Response response = client.newCall(request).execute();
         
+        String elJson = response.body().string();
+        
+        //cortar los corchetes
+        elJson = elJson.substring(1, elJson.length());
+        elJson = elJson.substring(0, elJson.length()-1);
+        
+        //crear u objeto de la clase Gson
+        Gson gson = new Gson();
+        Gatos gatos = gson.fromJson(elJson, Gatos.class);
+        
+        //redimensionar en caso de necesitar
+        Image image = null;
+        try{
+            URL url = new URL(gatos.getUrl());
+            image = ImageIO.read(url);
+            
+            ImageIcon fondoGato = new ImageIcon(image);
+            
+            if(fondoGato.getIconWidth() > 800){
+                //redimensionamos
+                Image fondo = fondoGato.getImage();
+                Image modificada = fondo.getScaledInstance(800, 600, java.awt.Image.SCALE_SMOOTH);
+                fondoGato = new ImageIcon(modificada);
+            }
+            
+        }catch(IOException e){
+               System.out.println(e);
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+
+    public GatosService() {
     }
     
     
